11d8d1f9435014fcbdc7ac7757f65a22f1832242
Sachini Dilrangi <sdilrangi58@gmail.com>
2025-11-22 20:33:20 +0530

    feat(submission): model class submission service

--- backend/submission-management-service/src/main/java/com/smartlms/submission_management_service/SubmissionManagementServiceApplication.java
+++ backend/submission-management-service/src/main/java/com/smartlms/submission_management_service/SubmissionManagementServiceApplication.java
@@ -1,9 +1,12 @@
 package com.smartlms.submission_management_service;
 
+import com.smartlms.submission_management_service.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileStorageProperties.class)
 public class SubmissionManagementServiceApplication {
 
 	public static void main(String[] args) {
