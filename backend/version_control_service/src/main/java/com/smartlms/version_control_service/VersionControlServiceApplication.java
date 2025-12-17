1f51f78b70b3525c3b0ff07b2c54042788c9026f
Sachini Dilrangi <sdilrangi58@gmail.com>
2025-12-17 19:50:11 +0530

    feat(version): setup springboot project for version control service

---
+++ backend/version_control_service/src/main/java/com/smartlms/version_control_service/VersionControlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartlms.version_control_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionControlServiceApplication.class, args);
+	}
+
+}
