a034290fb47f5b5c703e9bbf55c2088eba2fc864
Sachini Dilrangi <sdilrangi58@gmail.com>
2025-12-17 20:48:01 +0530

    feat(version): create configuration classes

--- backend/version_control_service/src/main/java/com/smartlms/version_control_service/VersionControlServiceApplication.java
+++ backend/version_control_service/src/main/java/com/smartlms/version_control_service/VersionControlServiceApplication.java
@@ -1,13 +1,18 @@
 package com.smartlms.version_control_service;
 
+import com.smartlms.version_control_service.config.VersionControlProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(VersionControlProperties.class)
+@EnableAsync
 public class VersionControlServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VersionControlServiceApplication.class, args);
 	}
-
 }
