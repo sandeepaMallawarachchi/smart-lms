3c91a713ebc99a0307efb054e53ac40f9698e126
Tashini S Hansanee <154216341+TasH-i@users.noreply.github.com>
2025-12-19 12:21:46 +0530

    start project and task dashboard

---
+++ frontend/app/projects-and-tasks/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectAndTasksManagementModule = () => {
+  return (
+    <div>Project And Tasks Management Module page</div>
+  )
+}
+
+export default ProjectAndTasksManagementModule
