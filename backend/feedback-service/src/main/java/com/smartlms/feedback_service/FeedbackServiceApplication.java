9934e04c508c5376a1574f43f1263c922afe692d
Sachini Dilrangi <sdilrangi58@gmail.com>
2025-12-29 22:26:50 +0530

    feat(feedback): update main class with annotations

--- backend/feedback-service/src/main/java/com/smartlms/feedback_service/FeedbackServiceApplication.java
+++ backend/feedback-service/src/main/java/com/smartlms/feedback_service/FeedbackServiceApplication.java
@@ -2,8 +2,12 @@ package com.smartlms.feedback_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
 public class FeedbackServiceApplication {
 
 	public static void main(String[] args) {
