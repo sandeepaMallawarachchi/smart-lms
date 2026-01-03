4b41ae6da69532bf6d3cc56e3a6277b6c7fa2b96
Sachini Dilrangi <sdilrangi58@gmail.com>
2026-01-03 19:45:00 +0530

    feat(integrity): setup new springboot project for integrity monitoring service

---
+++ backend/integrity-monitoring-service/src/main/java/com/example/integrity_monitoring_service/IntegrityMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.integrity_monitoring_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrityMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrityMonitoringServiceApplication.class, args);
+	}
+
+}
