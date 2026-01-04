1f4e0c36f92956a56435c905bd75118fd35d3709
Sachini Dilrangi <sdilrangi58@gmail.com>
2026-01-04 10:44:32 +0530

    feat(integrity): added necessary annotations on the main class

--- backend/integrity-monitoring-service/src/main/java/com/example/integrity_monitoring_service/IntegrityMonitoringServiceApplication.java
+++ backend/integrity-monitoring-service/src/main/java/com/example/integrity_monitoring_service/IntegrityMonitoringServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.integrity_monitoring_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class IntegrityMonitoringServiceApplication {
 
 	public static void main(String[] args) {
