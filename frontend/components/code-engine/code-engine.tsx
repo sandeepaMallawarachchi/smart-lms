f3e5455cb22862dd2600b36d0bde2a4bfc811020
Nipuna Sudaraka <sudaraka731@gmail.com>
2026-01-08 01:05:38 +0530

    code engine integration/test cases default view added

--- frontend/components/code-engine/code-engine.tsx
+++ frontend/components/code-engine/code-engine.tsx
@@ -1,29 +1,6 @@
 import CodeSpace from "@/components/code-engine/code-space"
 import { Assignment } from "@/types/types"
 
-// const data = {
-//   courseId: "6959ebb9aecdd635139e33ee",
-//   lecturerId: "695692c5370550efdf14b004",
-//   question: "Write a program that prints Hello World",
-//   projectType: "code", 
-//   language: "javascript",
-//   deadlineDate: "2024-01-01",
-//   deadlineTime: "12:00:00Z",
-//   options: {
-//     autoComplete: true,
-//     externalCopyPaste: false,
-//     internalCopyPaste: true,
-//     analytics: true
-//   },
-//   testCases: [
-//     {
-//       id: 1,
-//       input: "", 
-//       expectedOutput: "Hello World"
-//     }
-//   ]
-// }
-
 interface CodeEngineProps {
   defaultCode: string
   assignment: Assignment
